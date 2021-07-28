4343fe31d72f472721fda01bdf8628a67d98e295
Tristan Burgess <tburgessdev@gmail.com>
2021-07-27 22:46:00 -0400

    more build script adjustments for cpp

--- cpp/build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
-
-mkdir build
-pushd build
-cl /Zi /EHsc /I ..\generic_types\ /I ..\test_framework\ ..\epi\anagrams.cc
-popd 
--- cpp/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mkdir -p build && cd build
-clang++ -std=c++14 -g -pthread -I ../generic_types -I ../test_framework ../${1} -o ./
