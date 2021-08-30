248e4ab3206b3b16b64fa60f41242f7aa5636d16
Tristan Burgess <tburgessdev@gmail.com>
2021-08-30 08:45:12 -0400

    epi: Arrays - The Dutch national flag problem

--- cpp/epi/dutch_national_flag.cc
+++ cpp/epi/dutch_national_flag.cc
@@ -1,15 +1,55 @@
 #include <array>
 #include <vector>
+
 #include "generic_test.h"
 #include "test_failure.h"
 #include "timed_executor.h"
+using std::swap;
 using std::vector;
 typedef enum { kRed, kWhite, kBlue } Color;
 
+// O(n) time
+// O(1) space
 void DutchFlagPartition(int pivot_index, vector<Color>* A_ptr) {
-  // TODO - you fill in here.
-  return;
+  vector<Color>& A = *A_ptr;
+  Color pivot = A[pivot_index];
+  int lo = 0;
+  int mid = 0;
+  int hi = A.size() - 1;
+
+  while (mid <= hi) {
+    if (A[mid] < pivot) {
+      swap(A[lo++], A[mid++]);
+    } else if (A[mid] == pivot) {
+      mid++;
+    } else {
+      swap(A[mid], A[hi--]);
+    }
+  }
 }
+
+
+// O(n) time
+// O(1) space
+void DutchFlagPartitionBF(int pivot_index, vector<Color>* A_ptr) {
+  vector<Color>& A = *A_ptr;
+  Color pivot = A[pivot_index];
+
+  int lo = 0;
+  for (int i = 0; i < A.size(); i++) {
+    if (A[i] < pivot) {
+      swap(A[lo++], A[i]);
+    }
+  }
+
+  int hi = A.size() - 1;
+  for (int i = A.size() - 1; i >= 0 && A[i] >= pivot; i--) {
+    if (A[i] > pivot) {
+      swap(A[i], A[hi--]);
+    }
+  }
+}
+
 void DutchFlagPartitionWrapper(TimedExecutor& executor, const vector<int>& A,
                                int pivot_idx) {
   vector<Color> colors;
@@ -51,6 +91,6 @@ int main(int argc, char* argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"executor", "A", "pivot_idx"};
   return GenericTestMain(args, "dutch_national_flag.cc",
-                         "dutch_national_flag.tsv", &DutchFlagPartitionWrapper,
+                         "../test_data/epi/dutch_national_flag.tsv", &DutchFlagPartitionWrapper,
                          DefaultComparator{}, param_names);
 }
