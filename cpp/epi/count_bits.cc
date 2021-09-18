e52fadac2d1345b30e1e50cc6d1b7563922401bc
Tristan Burgess <tburgessdev@gmail.com>
2021-09-18 15:21:31 -0400

    epi: Primitive Types - Bootcamp - Counting bits

--- cpp/epi/count_bits.cc
+++ cpp/epi/count_bits.cc
@@ -1,13 +1,36 @@
 #include "generic_test.h"
 
+// Whiteboard: https://1drv.ms/u/s!AvHgsMnKfyusiIE4X-0ukpRgbTDMTg
+
+short CountBitsBF(unsigned int x) {
+  short count = 0;
+  while (x != 0) {
+    count += x & 1;
+    x >>= 1;
+  }
+  return count;
+}
+
 short CountBits(unsigned int x) {
-  // TODO - you fill in here.
-  return 0;
+  short count = 0;
+  while (x != 0) {
+    x ^= (x & ~(x - 1));
+    count++;
+  }
+  return count;
 }
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"x"};
-  return GenericTestMain(args, "count_bits.cc", "../test_data/epi/count_bits.tsv", &CountBits,
-                         DefaultComparator{}, param_names);
+
+  int r =
+      GenericTestMain(args, "count_bits.cc", "../test_data/epi/count_bits.tsv",
+                      &CountBitsBF, DefaultComparator{}, param_names);
+  if (r != 0) {
+    return r;
+  }
+  r = GenericTestMain(args, "count_bits.cc", "../test_data/epi/count_bits.tsv",
+                      &CountBits, DefaultComparator{}, param_names);
+  return r;
 }
