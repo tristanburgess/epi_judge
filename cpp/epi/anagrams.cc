591d1e2173e665960fb292077db6f0e2401f2762
Tristan Burgess <tburgessdev@gmail.com>
2021-09-20 12:41:59 -0400

    epi: Hash Tables - Bootcamp - Anagrams

--- cpp/epi/anagrams.cc
+++ cpp/epi/anagrams.cc
@@ -1,14 +1,31 @@
 #include <string>
+#include <unordered_map>
 #include <vector>
 
 #include "generic_test.h"
 
 using std::string;
+using std::unordered_map;
 using std::vector;
 
+// Whiteboard: https://1drv.ms/u/s!AvHgsMnKfyusiIE9n2uBFaY6a3mGPw
+
 vector<vector<string>> FindAnagrams(const vector<string>& dictionary) {
-  // TODO - you fill in here.
-  return {};
+  unordered_map<string, vector<string>> string_to_anagram_map;
+  for (const string& s : dictionary) {
+    string sorted_s(s);
+    sort(sorted_s.begin(), sorted_s.end());
+    string_to_anagram_map[sorted_s].emplace_back(s);
+  }
+
+  vector<vector<string>> anagrams;
+  for (const auto& p : string_to_anagram_map) {
+    if (p.second.size() >= 2) {
+      anagrams.emplace_back(p.second);
+    }
+  }
+
+  return anagrams;
 }
 
 int main(int argc, char* argv[]) {
