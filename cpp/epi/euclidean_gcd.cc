8fbcdd1114279bdb97aa0ae7f97cdedd8036dfc1
Tristan Burgess <tburgessdev@gmail.com>
2021-10-13 16:59:57 -0400

    epi: Recursion - Bootcamp - Euclidean GCD

--- cpp/epi/euclidean_gcd.cc
+++ cpp/epi/euclidean_gcd.cc
@@ -1,13 +1,18 @@
 #include "../test_framework/generic_test.h"
 
-long long Gcd(long long x, long long y) {
-  // TODO - you fill in here.
-  return 0;
+// Whiteboard: https://1drv.ms/u/s!AvHgsMnKfyusiIE_HeVtU98cFvo0PQ
+
+long long gcd(long long x, long long y) {
+  if (y == 0) {
+    return x;
+  } else {
+    return gcd(y, x % y);
+  }
 }
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
   std::vector<std::string> param_names{"x", "y"};
-  return GenericTestMain(args, "euclidean_gcd.cc", "gcd.tsv", &Gcd,
+  return GenericTestMain(args, "euclidean_gcd.cc", "../test_data/epi/gcd.tsv", &gcd,
                          DefaultComparator{}, param_names);
 }
