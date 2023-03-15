d9751f3353f17e03b1ef4addc62183cc1fb8818d
Tristan Burgess <tburgessdev@gmail.com>
2023-03-15 15:55:53 -0400

    epi cpp 7.1 string int interconversion

--- cpp/epi/string_integer_interconversion.cc
+++ cpp/epi/string_integer_interconversion.cc
@@ -4,13 +4,41 @@
 using std::string;
 
 string IntToString(int x) {
-  // TODO - you fill in here.
-  return "0";
+  string s;
+  bool sign = x < 0;
+
+  do {
+    s += (char)(abs(x % 10) + '0');
+    x /= 10;
+  } while (x != 0);
+
+  if (sign) {
+    s += '-';
+  }
+
+  return string(s.rbegin(), s.rend());
 }
 
 int StringToInt(const string& s) {
-  // TODO - you fill in here.
-  return 0;
+  if (s.size() == 0) {
+    // would probably throw an exception here/return an error
+    return 0;
+  }
+
+  int x = 0;
+  size_t idx = 0;
+
+  if (s[0] == '-') {
+    idx = 1;
+  }
+  for (size_t i = idx; i < s.size(); ++i) {
+    x = x * 10 + (int)(s[i] - '0');
+  }
+  if (s[0] == '-') {
+    x = -x;
+  }
+
+  return x;
 }
 
 void Wrapper(int x, const string& s) {
@@ -31,8 +59,10 @@ void Wrapper(int x, const string& s) {
 
 int main(int argc, char* argv[]) {
   std::vector<std::string> args{argv + 1, argv + argc};
+  std::string func_name = "IntToString,StringToInt";
   std::vector<std::string> param_names{"x", "s"};
+
   return GenericTestMain(args, "string_integer_interconversion.cc",
                          "../test_data/epi/string_integer_interconversion.tsv", &Wrapper,
-                         DefaultComparator{}, param_names);
+                         DefaultComparator{}, func_name, param_names);
 }
