0fba2dd37ad61d08c7f9237b4a10c7c92b710571
Tristan Burgess <tburgessdev@gmail.com>
2023-03-27 18:25:29 -0400

    epi java 14.1 intersect sorted arrays

--- java/epi/IntersectSortedArrays.java
+++ java/epi/IntersectSortedArrays.java
@@ -1,14 +1,39 @@
 package epi;
 import test_framework.EpiTest;
 import test_framework.GenericTest;
-import java.util.List;
-public class IntersectSortedArrays {
-  @EpiTest(testDataFile = "../test_data/epi/intersect_sorted_arrays.tsv")
 
-  public static List<Integer> intersectTwoSortedArrays(List<Integer> A,
-                                                       List<Integer> B) {
-    // TODO - you fill in here.
-    return null;
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntersectSortedArrays {
+
+  @EpiTest(testDataFile = "../test_data/epi/intersect_sorted_arrays.tsv")
+  public static List<Integer> intersectTwoSortedArrays(List<Integer> a,
+                                                       List<Integer> b) {
+      List<Integer> out = new ArrayList<>();
+
+      int aIdx = 0;
+      int bIdx = 0;
+      Integer curUniq = null;
+
+      while (aIdx < a.size() && bIdx < b.size()) {
+        Integer aC = a.get(aIdx);
+        Integer bC = b.get(bIdx);
+
+        if (aC.compareTo(bC) == 0 && (curUniq == null || aC.compareTo(curUniq) != 0)) {
+          out.add(aC);
+          curUniq = aC;
+          aIdx++;
+          bIdx++;
+        } else if (aC.compareTo(bC) > 0) {
+          bIdx++;
+        } else {
+          aIdx++;
+        }
+      }
+
+      return out;
+      
   }
 
   public static void main(String[] args) {
