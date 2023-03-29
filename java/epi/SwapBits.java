a88c493e30005ad9cc22f8d817f3dd7abc22d697
Tristan Burgess <tburgessdev@gmail.com>
2023-03-29 11:33:51 -0400

    epi java 5.2 swap bits

--- java/epi/SwapBits.java
+++ java/epi/SwapBits.java
@@ -1,11 +1,25 @@
 package epi;
 import test_framework.EpiTest;
 import test_framework.GenericTest;
+
 public class SwapBits {
+
+  @EpiTest(testDataFile = "../test_data/epi/swap_bits.tsv")
+  public static long swapBitsCond(long x, int i, int j) {
+    if (((x >>> i) & 0x1) != ((x >>> j) & 0x1)) {
+      return x ^ ((0x1L << i) | (0x1L << j));
+    }
+
+    return x;
+  }
+
   @EpiTest(testDataFile = "../test_data/epi/swap_bits.tsv")
   public static long swapBits(long x, int i, int j) {
-    // TODO - you fill in here.
-    return 0;
+    long xi = (x >>> i) & 0x1;
+    long xj = (x >>> j) & 0x1;
+    long xij = xi ^ xj;
+
+    return x ^ ((xij << i) | (xij << j));
   }
 
   public static void main(String[] args) {
