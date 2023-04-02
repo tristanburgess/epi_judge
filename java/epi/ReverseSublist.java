2b755a5080335468126568925cdd42935a73bee9
Tristan Burgess <tburgessdev@gmail.com>
2023-04-01 21:25:21 -0400

    epi java 8.2 reverse sublist

--- java/epi/ReverseSublist.java
+++ java/epi/ReverseSublist.java
@@ -5,12 +5,30 @@ import test_framework.EpiTest;
 import test_framework.GenericTest;
 
 public class ReverseSublist {
-  @EpiTest(testDataFile = "../test_data/epi/reverse_sublist.tsv")
 
-  public static ListNode<Integer> reverseSublist(ListNode<Integer> L, int start,
-                                                 int finish) {
-    // TODO - you fill in here.
-    return null;
+  @EpiTest(testDataFile = "../test_data/epi/reverse_sublist.tsv")
+  public static ListNode<Integer> reverseSublist(ListNode<Integer> L, int start, int finish) {
+    
+    if (L == null || start >= finish) {
+      return L;
+    }       
+  
+    ListNode<Integer> d = new ListNode<Integer>(0, L);
+    ListNode<Integer> h = d;
+                              
+    for (int i = 1; i < start; i++) {
+      h = h.next;
+    }
+
+    ListNode<Integer> p = h.next;
+    for (int i = 0; i < (finish - start); i++) {
+      ListNode<Integer> c = p.next;
+      p.next = c.next;
+      c.next = h.next;
+      h.next = c;
+    }
+
+    return d.next;
   }
 
   public static void main(String[] args) {
