df1036f2485b53c413c1697fe99c0c8331dad97a
Tristan Burgess <tburgessdev@gmail.com>
2023-04-10 08:58:29 -0400

    epi java 15.2 search first greater value in BST

--- java/epi/SearchFirstGreaterValueInBst.java
+++ java/epi/SearchFirstGreaterValueInBst.java
@@ -6,11 +6,41 @@ import test_framework.GenericTest;
 
 public class SearchFirstGreaterValueInBst {
 
-  public static BstNode<Integer> findFirstGreaterThanK(BstNode<Integer> tree,
-                                                       Integer k) {
-    // TODO - you fill in here.
-    return null;
+  private static BstNode<Integer> findRecurHelper(BstNode<Integer> tree, 
+                                                  Integer k, 
+                                                  BstNode<Integer> candidate) {
+    if (tree == null) {
+      return candidate;
+    }
+
+    if (tree.data <= k) {
+      return findRecurHelper(tree.right, k, candidate);
+    }
+
+    return findRecurHelper(tree.left, k, tree);
   }
+
+  public static BstNode<Integer> findFirstGreaterThanKRecur(BstNode<Integer> tree,
+                                                            Integer k) {
+      return findRecurHelper(tree, k, null);                                                    
+  }
+
+  public static BstNode<Integer> findFirstGreaterThanK(BstNode<Integer> tree,
+                                                      Integer k) {
+    BstNode<Integer> candidate = null;
+    
+    while (tree != null) {
+      if (tree.data <= k) {
+        tree = tree.right;
+      } else {
+        candidate = tree;
+        tree = tree.left;
+      }
+    }
+
+    return candidate;
+  }
+
   @EpiTest(testDataFile = "../test_data/epi/search_first_greater_value_in_bst.tsv")
   public static int findFirstGreaterThanKWrapper(BstNode<Integer> tree,
                                                  Integer k) {
