377e4387cd6e1ca29b31791b39313db0f82f30e8
Tristan Burgess <tburgessdev@gmail.com>
2023-04-14 10:07:36 -0400

    epi java 5.3 reverse bits

--- java/epi/ReverseBits.java
+++ java/epi/ReverseBits.java
@@ -1,11 +1,41 @@
 package epi;
 import test_framework.EpiTest;
 import test_framework.GenericTest;
+
 public class ReverseBits {
+
+  private static long reverseBitsW(long x, int w) {
+    int hi = w - 1;
+    int lo = 0;
+
+    while (hi > lo) {
+      if (((x >>> hi) & 0x1) != ((x >>> lo) & 0x1)) {
+        x ^= (0x1L << hi) | (0x1L << lo);
+      }
+      hi--;
+      lo++;
+    }
+
+    return x;
+  }
+
   @EpiTest(testDataFile = "../test_data/epi/reverse_bits.tsv")
   public static long reverseBits(long x) {
-    // TODO - you fill in here.
-    return 0;
+    return reverseBitsW(x, 64);
+  }
+
+  @EpiTest(testDataFile = "../test_data/epi/reverse_bits.tsv")
+  public static long reverseBitsCache(long x) {
+    long[] c = new long[65536];
+    for (int i = 0; i < 65536; i++) {
+      c[i] = reverseBitsW(i, 16);
+    }
+
+    long y = c[(int)(x & 0xFFFF)] << 48;
+    y |= c[(int)((x >>> 16) & 0xFFFF)] << 32;
+    y |= c[(int)((x >>> 32) & 0xFFFF)] << 16;
+    y |= c[(int)((x >>> 48) & 0xFFFF)];
+    return y;
   }
 
   public static void main(String[] args) {
